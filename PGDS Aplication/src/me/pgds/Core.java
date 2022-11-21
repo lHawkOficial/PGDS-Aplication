dd72bc86da3ca01b9f93e4389ddea43927d9ce4e
lHawkOficial <luanvictorchagas2015@gmail.com>
2022-11-21 13:18:11 -0300

    updated

--- PGDS Aplication/src/me/pgds/Core.java
+++ PGDS Aplication/src/me/pgds/Core.java
@@ -1,10 +1,15 @@
 package me.pgds;
 
-public class Core {
+import lombok.Getter;
+import me.pgds.utils.WindowCore;
 
+public class Core {
+	
+	@Getter
+	private static WindowCore window;
+	
 	public static void main(String[] args) {
-		
-		
+		window = new WindowCore().init();
 	}
 	
 }
---
+++ PGDS Aplication/src/me/pgds/utils/WindowCore.java
@@ -0,0 +1,39 @@
+package me.pgds.utils;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import lombok.Getter;
+import me.pgds.Core;
+
+@Getter
+public class WindowCore extends JFrame {
+	
+	private static final long serialVersionUID = -5140442525033186243L;
+
+	public WindowCore() {
+		setResizable(false);
+		setSize(1280, 720);
+		setAlwaysOnTop(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(Color.GRAY.darker());
+		setTitle("PEGADAS PRÉ-MOLDADOS");
+	}
+	
+	public WindowCore init() {
+		setVisible(true);
+		update(getGraphics());
+		return this;
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public static WindowCore get() {
+		return Core.getWindow();
+	}
+			
+}

646f2adcf4d298b59f44aee73c4412d0cb44d223
lHawkOficial <luanvictorchagas2015@gmail.com>
2022-11-30 12:39:03 -0300

    updated

--- PGDS Aplication/src/me/pgds/Core.java
+++ PGDS Aplication/src/me/pgds/Core.java
@@ -1,22 +1,13 @@
 package me.pgds;
 
-import java.awt.Color;
 
-import me.pgds.objects.utils.Button;
+
 import me.pgds.utils.WindowCore;
-import me.pgds.utils.frames.MainFrame;
 
 public class Core {
 	
 	public static void main(String[] args) {
 		new WindowCore().init();
-		new MainFrame(()->{
-			int size = 0;
-			for (int i = 0; i < 27; i++) {
-				Button button = new Button("Botão " + i, Color.YELLOW.darker().darker(), Color.white, 200, 25, 2, 2+size+i);
-				size+=button.getHeight();
-			}
-		}).run();
 	}
 	
 }
